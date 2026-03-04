3ac35db3b17cbba56b0ff26b75bdf42fd650828c
Danish Banga <d_banga@yahoo.com>
2026-03-04 15:53:04 -0800

    Fix supabaseClient encoding

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,12 +1,6 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2868
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;}
-{\colortbl;\red255\green255\blue255;\red108\green0\blue181;\red0\green0\blue0;\red32\green36\blue45;
-\red15\green112\blue1;\red16\green19\blue24;\red4\green57\blue181;\red14\green110\blue109;}
-{\*\expandedcolortbl;;\cssrgb\c50588\c0\c76078;\cssrgb\c0\c0\c0;\cssrgb\c16863\c18824\c23137;
-\cssrgb\c0\c50196\c0;\cssrgb\c7843\c9412\c12157;\cssrgb\c0\c31765\c76078;\cssrgb\c0\c50196\c50196;}
-\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720\partightenfactor0
+import { createClient } from '@supabase/supabase-js'
 
-\f0\fs24 \cf2 \expnd0\expndtw0\kerning0
-\outl0\strokewidth0 \strokec2 import\cf0 \strokec3  \cf4 \strokec4 \{\cf0 \strokec3  createClient \cf4 \strokec4 \}\cf0 \strokec3  \cf2 \strokec2 from\cf0 \strokec3  \cf5 \strokec5 '@supabase/supabase-js'\cf0 \strokec3  \cf2 \strokec2 export\cf0 \strokec3  \cf2 \strokec2 const\cf0 \strokec3  supabase \cf6 \strokec6 =\cf0 \strokec3  \cf7 \strokec7 createClient\cf4 \strokec4 (\cf2 \strokec2 import\cf4 \strokec4 .\cf0 \strokec3 meta\cf4 \strokec4 .\cf0 \strokec3 env\cf4 \strokec4 .\cf8 \strokec8 VITE_SUPABASE_URL\cf4 \strokec4 ,\cf0 \strokec3  \cf2 \strokec2 import\cf4 \strokec4 .\cf0 \strokec3 meta\cf4 \strokec4 .\cf0 \strokec3 env\cf4 \strokec4 .\cf8 \strokec8 VITE_SUPABASE_ANON_KEY\cf0 \strokec3  \cf4 \strokec4 )}
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
